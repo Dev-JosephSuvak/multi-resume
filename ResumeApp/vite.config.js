4900cef241aeaabf6cf2e8d3fd35111d77474e05
Joseph Suvak <JosephSuvakDevelopment@gmail.com>
2026-03-19 12:19:55 -0500

    fix outdated netlify asset deployment config

--- ResumeApp/vite.config.js
+++ ResumeApp/vite.config.js
@@ -8,14 +8,6 @@ export default defineConfig({
   build: {
     outDir: '../wwwroot/dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: './ClientApp/index.html',
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
   },
   server: {
     port: 5173
